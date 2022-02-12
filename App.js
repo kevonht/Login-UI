a874b5a42622d220a0c882d9cb5c2cfd57aec5f2
kevonht <kevonht@gmail.com>
2022-02-11 20:57:04 -0600

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,138 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput , Dimensions} from 'react-native';
+import Svg, { Path, Defs, LinearGradient, Stop } from "react-native-svg"
+
+
+import ButtonGradient from './ButtonGradient'
+
+//capturar el ancho de la pantalla y algo si se ocupa
+const { width, height } = Dimensions.get('window')
+// funcion del svg
+
+
+
+function SvgTop() {
+  return (
+    <Svg
+    width={500}
+    height={324}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <Path
+      d="M297.871 315.826c73.405 13.896 165.338-13.964 202.129-29.63V230H1.326v63.5c69.15-42.913 204.789 4.957 296.545 22.326z"
+      fill="url(#prefix__paint0_linear_103:6)"
+      fillOpacity={0.5}
+    />
+    <Path
+      d="M237.716 308.627C110.226 338.066 30.987 318.618 0 304.77V0h500v304.77c-43.161-12.266-134.794-25.581-262.284 3.857z"
+      fill="url(#prefix__paint1_linear_103:6)"
+    />
+    <Defs>
+      <LinearGradient
+        id="prefix__paint0_linear_103:6"
+        x1={492.715}
+        y1={231.205}
+        x2={480.057}
+        y2={364.215}
+        gradientUnits="userSpaceOnUse"
+      >
+        <Stop stopColor="#FFB677" />
+        <Stop offset={1} stopColor="#FF3CBD" />
+        
+      </LinearGradient>
+      
+      <LinearGradient
+        id="prefix__paint1_linear_103:6"
+        x1={7.304}
+        y1={4.155}
+        x2={144.016}
+        y2={422.041}
+        gradientUnits="userSpaceOnUse"
+      >
+        
+        <Stop stopColor="#FFB677" />
+        <Stop offset={1} stopColor="#FF3CBD" />
+        
+      </LinearGradient>
+    </Defs>
+    
+  </Svg>
+  )
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  
+  
+  return(
+
+    <View style={styles.mainContainer}>
+      <View style={styles.containerSVG}>
+        
+        <SvgTop/>
+        
+      </View>
+      <View style={styles.container}>
+
+        <Text style={styles.titulo}>Hello</Text>
+        <Text style={styles.subtitle}> Sign to your account</Text>
+        <TextInput 
+          placeholder="Jhon@gmail.com"
+          style={styles.imput}
+        />
+        <TextInput 
+          placeholder='Password'
+          secureTextEntry={true}
+          style={styles.imput}
+        />
+        <Text style={styles.forgotPassword}>Forgot your password?</Text>
+        < ButtonGradient />
+        <Text style={styles.forgotPassword}>Don't have an account?</Text>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer:{
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f1f1f1',
+  },
+  container: {
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+  containerSVG:{
+    width: width,
+    justifyContent:'flex-start',
+    alignItems: 'center'
+  },
+  titulo:{
+    fontSize: 60,
+    color:'#34434D',
+    fontWeight:'bold'
+  }, 
+  subtitle:{
+    fontSize:20,
+    color:'gray'
+  },
+  imput:{
+   
+
+    width: '80%',
+    padding:10,
+    marginTop: 20,
+    height:50,
+    borderRadius:30,
+    backgroundColor:'#fff',
+    paddingStart: 30
+  },
+  forgotPassword: {
+    fontSize: 14,
+    color: 'gray',
+    marginTop: 20
+  },
+
 });
